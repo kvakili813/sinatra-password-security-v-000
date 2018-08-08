d4bb002d994d3ee8454bdcbf2088cf4cc2294f39
Kayvan Vakili <kvakili813@gmail.com>
2018-08-08 05:26:50 +0000

    automatically backed up by learn

--- db/migrate/20180314210852_create_users.rb
+++ db/migrate/20180314210852_create_users.rb
@@ -1,9 +1,11 @@
-def up
-        create_table :users do |t|
-            t.string :username
-            t.string :password_digest
-        end
+class CreateUsers < ActiveRecord::Migration[5.1]
+
+  def up
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
     end
+  end
 
     def down
         drop_table :users
